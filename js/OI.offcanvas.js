28ead8055a15aa34cfd2e224d487638aba173ac9
Joe Weber <wosephjeber@gmail.com>
2015-10-02 18:32:03 -0500

    Basic functionality in place - open and close drawer

--- js/OI.offcanvas.js
+++ js/OI.offcanvas.js
@@ -9,8 +9,38 @@
 var OffCanvas = function(elem, options) {
   var _this = this;
   this.elem = $(elem);
+  this.status = 'closed';
 
   this.options = $.extend({
-    
+    trigger: $('.offcanvas-trigger'),
+    overlay: $('.offcanvas-overlay')
   }, options);
+  
+  this.open = function() {
+    $('body').addClass('animate-drawer');
+    _this.elem.addClass('animate');
+    setTimeout(function() {
+      $('body').addClass('open-drawer');
+      _this.elem.addClass('show');
+    }, 1);
+    _this.status = 'open';
+  };
+  
+  this.close = function() {
+    $('body').removeClass('open-drawer');
+    _this.elem.removeClass('show');
+    setTimeout(function() {
+      $('body').removeClass('animate-drawer');
+      _this.elem.removeClass('animate');
+    }, 500);
+    _this.status = 'closed';
+  };
+  
+  this.options.trigger.on('click', function() {
+    if (_this.status === 'open') {
+      _this.close();
+    } else {
+      _this.open();
+    }
+  });
 };
