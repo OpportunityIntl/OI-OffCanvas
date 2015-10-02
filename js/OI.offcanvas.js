22dd40c1d1667493fcbfabca33fd064bb71f1563
Joe Weber <wosephjeber@gmail.com>
2015-10-02 18:40:07 -0500

    Cache $('body') for fewer lookups

--- js/OI.offcanvas.js
+++ js/OI.offcanvas.js
@@ -8,6 +8,7 @@
 
 var OffCanvas = function(elem, options) {
   var _this = this;
+  var body = $('body');
   this.elem = $(elem);
   this.status = 'closed';
 
@@ -17,20 +18,20 @@ var OffCanvas = function(elem, options) {
   }, options);
   
   this.open = function() {
-    $('body').addClass('animate-drawer');
+    body.addClass('animate-drawer');
     _this.elem.addClass('animate');
     setTimeout(function() {
-      $('body').addClass('open-drawer');
+      body.addClass('open-drawer');
       _this.elem.addClass('show');
     }, 1);
     _this.status = 'open';
   };
   
   this.close = function() {
-    $('body').removeClass('open-drawer');
+    body.removeClass('open-drawer');
     _this.elem.removeClass('show');
     setTimeout(function() {
-      $('body').removeClass('animate-drawer');
+      body.removeClass('animate-drawer');
       _this.elem.removeClass('animate');
     }, 500);
     _this.status = 'closed';
